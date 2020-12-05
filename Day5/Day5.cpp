1ed73ee52ec372f6015973874d9d01b2fe99a163
FelixHuyghe <46262936+FelixHuyghe@users.noreply.github.com>
2020-12-05 16:59:10 +0100

    Day5

--- Day5/Day5.cpp
+++ Day5/Day5.cpp
@@ -1,20 +1,38 @@
-// Day5.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+//code gebruikt van het interenet voor deel 2 want geen zin in manueel checks schrijven :(
+using namespace std;
+#define _crt_secure_no_warnings
+#include <vector>
+#include <fstream>
+#include <string>
+#include <sstream>
 #include <iostream>
+#include <cmath>
+#include <algorithm>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	fstream myfile("input.txt");
+	int counter = 0;
+	string line;
+	int numbofnecccomponents = 0;
+	vector<int> seats;
+	int curRow = 0, curCol = 0;
+	while (getline(myfile, line)) {
+		curRow = 0, curCol = 0;
+		for (int i = 0; i < 7; i++)
+			if (line[i] == 'B')
+				curRow += 1 << (7 - i - 1);
+		for (int i = 0; i < 3; i++)
+			if (line[7+i] == 'R')
+				curCol += 1 << (3 - i - 1);
+		seats.push_back(curRow * 8 + curCol);
+	}
+	//cout << *max_element(seats.begin(), seats.end()) << endl;
+	sort(seats.begin(), seats.end());
+	for (int i = 0; i < seats.size() - 1; i++)
+	{
+		if (seats[i] + 1 != seats[i + 1])
+			cout << seats[i] + 1 << endl;
+	}
+	printf("#############");
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
