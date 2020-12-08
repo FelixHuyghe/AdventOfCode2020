97271bdfcc2824e78014b6ca7a8e13ce88afff1d
FelixHuyghe <46262936+FelixHuyghe@users.noreply.github.com>
2020-12-08 10:59:38 +0100

    Day8

---
+++ Day8/Day8.cpp
@@ -0,0 +1,71 @@
+//code gebruikt van het interenet voor deel 2 want geen zin in manueel checks schrijven :(
+using namespace std;
+#define _crt_secure_no_warnings
+#include <vector>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <bitset>
+
+int main()
+{
+	fstream myfile("input.txt");
+	int counter = 0;
+	string line;
+	vector<pair<string, int>> instructions;
+	string s;
+	int operand;
+	while (getline(myfile, line)) {
+		stringstream ss(line);
+		ss >> s >> operand;
+		instructions.push_back(make_pair(s, operand));
+	}
+	int numberToFind = instructions.size();
+	bool found = false;
+	int acc = 0;
+	int i = -1;
+	while (!found) {
+		i++;
+		if (instructions[i].first == "jmp")
+			instructions[i].first = "nop";
+		else if (instructions[i].first == "nop")
+			instructions[i].first = "jmp";
+		else
+			continue;
+		bitset<1000> visited;
+		acc = 0;
+		int pos = 0;
+		while (1) {
+			if (pos == numberToFind) {
+				found = true;
+				break;
+			}
+			if (visited[pos])
+				break;
+			else
+				visited[pos] = true;
+			string ins = instructions[pos].first;
+			int op = instructions[pos].second;
+			if (ins == "nop") {
+				pos++;
+				continue;
+			}
+			else if (ins == "acc") {
+				pos++;
+				acc += op;
+			}
+			else if (ins == "jmp") {
+				pos += op;
+			}
+		}
+		if (instructions[i].first == "jmp")
+			instructions[i].first = "nop";
+		else if (instructions[i].first == "nop")
+			instructions[i].first = "jmp";
+	}
+	cout << acc << endl;
+	printf("#############");
+}
